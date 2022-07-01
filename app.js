dae7cc8c7748d8b9e4c22dc52e47fab750bb2b54
brendaetot <brendayah256@gmail.com>
2022-07-01 16:42:44 +0300

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
 
+const{
+  PORT=5400,
+  NODE_ENV='development',
+  
+
+
+}=process.env;
+const IN_PROD=NODE_ENV==='production';
+
 const app = express();
 
 const publicDirectory = path.join(__dirname, "./public");
@@ -10,6 +19,6 @@ app.set("view engine", "hbs");
 app.get("/", (req, res) => {
     res.render("index");
   });
-app.listen(4003, () => {
-    console.log("server started on port 4003")
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
 } );
